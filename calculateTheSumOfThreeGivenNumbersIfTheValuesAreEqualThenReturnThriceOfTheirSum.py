b5cb93d4dc3d12f6741f36a553a6152b7d95ed9e
Dev Chouhan <68985102+dev-chouhan@users.noreply.github.com>
2022-03-03 11:49:44 +0530

    Create calculateTheSumOfThreeGivenNumbersIfTheValuesAreEqualThenReturnThriceOfTheirSum.py

---
+++ calculateTheSumOfThreeGivenNumbersIfTheValuesAreEqualThenReturnThriceOfTheirSum.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter Second number: "))
+num3 = int(input("Enter Third number: "))
+
+if num1 == num2 == num3:
+    print((num1+num2+num3)*3)
+else:
+    print((num1+num2+num3))
